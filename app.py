d0e81bef7e422e5c0851d52d19bc1c3dda91312d
Ftx999 <90642087+Ftx999@users.noreply.github.com>
2025-07-10 22:52:45 +0800

    Update app.py

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ def run_signal():
     else:
         return "<br>".join(output)
 
-
+import os
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 10000))
     app.run(host='0.0.0.0', port=port)
